1937cc74266d718ec6e96e9e7e287becfc59b548
baradpraveenkumar2 <165669973+baradpraveenkumar2@users.noreply.github.com>
2024-09-27 14:40:34 +0530

    Update stream1.py

--- stream1.py
+++ stream1.py
@@ -2,7 +2,7 @@ import streamlit as st
 from transformers import GPT2Tokenizer, GPT2LMHeadModel
 import pandas as pd
 import os
-from chat_bot import store_csv_in_db, generate_sql_query, run_sql_query, generate_visualization, split_query_into_parts, COLUMN_NAMES, is_visualization_query, is_table_query
+from groqm import store_csv_in_db, generate_sql_query, run_sql_query, generate_visualization, split_query_into_parts, COLUMN_NAMES, is_visualization_query, is_table_query
 from langchain_experimental.agents import create_csv_agent
 from langchain.llms import HuggingFaceHub
 from dotenv import load_dotenv
